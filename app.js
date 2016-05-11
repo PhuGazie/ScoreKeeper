4ac508605a05893d99a3425e96d21cfe5f55ec1d
PhuGazie <jennyphu@gmail.com>
2016-05-10 18:56:39 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
 
   var sum = 0;
-  $("#score").html(sum);
+  $("#score").html(sum + " Points");
 
   $(document).on("click", "#increase-5", function() {
     sum = sum + 5;
-    $("#score").html(sum);
+    $("#score").html(sum + " Points");
   });
 
   $(document).on("click", "#decrease-5", function() {
     sum = sum - 5;
-    $("#score").html(sum);
+    $("#score").html(sum + " Points");
 
   });
 
   $(document).on("click", "#submit-custom-score", function (){
       var custom = parseInt($("#custom-score").val());
-      $("#score").html(custom);
-      sum = custom + sum;
+      $("#score").html(custom + " Points");
+      sum = custom;
   });
 
 
